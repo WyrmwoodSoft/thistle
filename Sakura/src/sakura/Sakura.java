bf65c40b73cb6ae28ff25d9b85970b187f0fe12d
Matt <metropolitan.matt@gmail.com>
2016-09-22 22:14:29 -0400

    My first git post, its a .java file.

---
+++ Sakura/src/sakura/Sakura.java
@@ -0,0 +1,14 @@
+
+package sakura;
+import javax.swing.JOptionPane;
+
+public class Sakura 
+{
+    public static void main(String[] args)
+    {
+        String tomodachi;
+        JOptionPane.showMessageDialog(null, "こにちわ ともだちよ!");
+ 
+    }
+    
+}
